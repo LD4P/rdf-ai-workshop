3b406c4bcef7c1b6fd61ea705e2436362e5f7989
Jeremy Nelson <jermnelson@gmail.com>
2021-07-17 19:28:46 -0600

    Working FastAI started answers

--- helpers.py
+++ helpers.py
@@ -1,19 +1,23 @@
 import json
 import io
+import random
+import urllib.parse
+
 from datetime import datetime
 from typing import Dict, List, Optional
 from zipfile import ZipFile
 
-
+import numpy as np
 import pandas as pd
-
 import kglab
 import rdflib
 import requests
 
+from fastcore.foundation import L
+
 NAMESPACES = {
     "bf": "http://id.loc.gov/ontologies/bibframe/",
-    "bflc":"",
+    "bflc": "http://id.loc.gov/ontologies/bflc/",
     "mads": "http://www.loc.gov/mads/rdf/v1#",
     "skos": "http://www.w3.org/2004/02/skos/core#",
     "sinopia": "http://sinopia.io/vocabulary/"
@@ -45,16 +49,21 @@ def harvest(api_url: str) -> rdflib.Graph:
             return
         jsonld = json.dumps(resource.pop("data")).encode()
         try:
-            graph.parse(data=jsonld, format="json-ld")
+            nt_rdf = preprocess(jsonld, resource.get('uri'))
+            graph.parse(data=nt_rdf, format="nt")
         except Exception as error:
             print(f"Failed to parse {resource}\n{error}")
             return
+    count = 0
     graph = rdflib.Graph()
-    graph.namespace_manager.bind("sinopia", SINOPIA)
+    for ns, uri in NAMESPACES.items():
+        graph.namespace_manager.bind(ns, uri)
     initial = requests.get(api_url)
     for row in initial.json().get("data"):
         add_resource(row)
     next_link = initial.json().get("links").get("next")
+    if next_link is None:
+        return graph
     while 1:
         result = requests.get(next_link)
         if result.status_code > 300:
@@ -67,30 +76,91 @@ def harvest(api_url: str) -> rdflib.Graph:
             break
         for row in payload.get("data"):
             add_resource(row)
+            if not count%25 and count > 0:
+                print(".", end="")
+            if not count%100:
+                print(f"{count:,}", end="")
+            count += 1
         next_link = new_next
     return graph
-
-def from_zip_url(zip_url: str) -> rdflib.Graph:
-    """Takes the url to the export zip file from a Sinopia environment, extracts all
-    RDF and returns an instance of the Knowledge Graph
-
-    @param zip_url -- URL to the zip export file
-    """
-    zip_result = requests.get(zip_url)
-    graph = rdflib.Graph()
-    with ZipFile(io.BytesIO(zip_result.content)) as zip_file:
-        for zip_info in zip_file.infolist():
-            if zip_info.file_size < 1 or zip_info.filename.endswith('log'):
-                continue
-            with zip_file.open(zip_info) as zip_extract:
-                raw_data = zip_extract.read()
-                try:
-                    resource = json.loads(raw_data)
-                    if 'data' in resource:
-                        graph.parse(data=json.dumps(resource.get('data')), format='json-ld')
-                except  Exception as error:
-                    print(f"Failed to parse {zip_info.filename}\n{error}")
-
-    return graph
         
-                 
+def preprocess(jsonld: str, resource_uri: str) -> str:
+    graph = rdflib.Graph()
+    graph.parse(data=jsonld, format='json-ld')
+    for s,p,o in graph:
+        if isinstance(s, rdflib.URIRef) and not rdflib.term._is_valid_uri(s):
+            new_s = fix_invalid_url(s)
+            for pred, obj in graph.predicate_objects(s):
+                graph.remove((s,pred,obj))
+                graph.add((new_s,pred,obj))
+        if isinstance(o, rdflib.URIRef) and not rdflib.term._is_valid_uri(o):
+            new_o = fix_invalid_url(o)
+            for subj, pred in graph.subject_predicates(o):
+                graph.remove((subj, pred, o))
+                graph.add((subj, pred, new_o))
+    # Need for downstream ML applications to have unique bnodes
+    resource_parts = urllib.parse.urlsplit(resource_uri)
+    basepath = f"{resource_parts.path}#"
+    graph = graph.skolemize(authority=resource_uri,
+                            basepath=basepath)
+    return graph.serialize(format='nt').decode()
+
+def fix_invalid_url(uri: rdflib.URIRef) -> rdflib.URIRef:
+    """Parses URI and attempts to quote parts in order to create valid URI
+    
+    @param uri -- URIRef
+    """
+    parse_result = urllib.parse.urlsplit(str(uri))
+    fixed_parts = (parse_result.scheme,
+                   parse_result.netloc,
+                   urllib.parse.quote_plus(parse_result.path),
+                   urllib.parse.quote_plus(parse_result.query),
+                   urllib.parse.quote_plus(parse_result.fragment))
+    fixed_url = urllib.parse.urlunsplit(fixed_parts)
+    return rdflib.URIRef(fixed_url)
+
+def predicate_row(subject: rdflib.URIRef, graph: rdflib.Graph) -> dict:
+    """Returns a dictionary of weighted predicates using a subject
+
+    @param subject -- Subject URI
+    @param graph -- RDF graph
+    """
+    output = { 'uri': str(subject) }
+    predicates = [pred for pred in graph.predicates(subject=subject)]
+    for predicate in predicates:
+        # Use value of sinopia:hasResourceTemplate as our dependent variable
+        if predicate == SINOPIA.hasResourceTemplate:
+            template_name = str(graph.value(subject=subject, predicate=predicate))
+            output['template'] = str(graph.value(subject=subject, predicate=predicate))
+        pred_key = str(predicate)
+        if pred_key in output:
+            output[pred_key] += 1.
+        else:
+            output[pred_key] = 1.
+    return output
+
+def create_splits(df, valid_pct=0.2):
+    train, valid = L(), L()
+    for row_values in df.groupby("template").indices.values():
+        # Shuffle group
+        random.shuffle(row_values)
+        # Randomly add first or second to each train and valid lists to 
+        # guarantee label is in both
+        if random.random() <= 0.5:
+            train.append(row_values[0])
+            valid.append(row_values[1])
+        else:
+            train.append(row_values[1])
+            valid.append(row_values[0])
+        # Iterate through the rest of the group and add to train and valid lists
+        # based on valid percentage
+        for i, v in enumerate(row_values):
+            if i < 2:
+                continue
+            if random.random() <= valid_pct:
+                valid.append(v)
+            else:
+                train.append(v)
+    return (train, valid)
+          
+              
