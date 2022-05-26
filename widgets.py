19014d041951f57ab1926cda137d4720caaeb2b6
Jeremy Nelson <jermnelson@gmail.com>
2022-05-26 10:23:16 -0600

    Widget now dynamically loading groups from env, content updates, removes old nb and adds new

--- widgets.py
+++ widgets.py
@@ -1,61 +1,58 @@
-
 import json
+import logging
 import pathlib
 
+from functools import cache
+
+import requests
 import ipywidgets as widgets
 
 data = pathlib.Path("data")
 
 data_dropdown = widgets.Dropdown(
     options=[row.name for row in data.iterdir()],
-    description='Data Files:',
-    disabled=False
+    description="Data Files:",
+    disabled=False,
 )
 
-sinopia_groups = {
-    'All': None,
-    'Cornell University': 'cornell',
-    'Duke University': 'duke',
-    'Frick Art Reference Library': 'frick',
-    'Harvard University': 'harvard',
-    'Library of Congress': 'dlc',
-    'Music Library Association': 'mla',
-    'National Library of Medicine': 'nlm',
-    'Northwestern University': 'northwestern',
-    'PCC': 'pcc',
-    'Princeton University': 'princeton',
-    'Stanford University': 'stanford',
-    'Texas A&M University': 'tamu',
-    'University of Alberta': 'alberta',
-    'University of California, Davis': 'ucdavis',
-    'University of California, San Diego': 'ucsd',
-    'University of Chicago': 'chicago',
-    'University of Colorado, Boulder': 'boulder',
-    'University of Michigan': 'michigan',
-    'University of Minnesota': 'minnesota',
-    'University of Pennsylvania': 'penn',
-    'University of Texas, Austin, Harry Ransom Center': 'hrc',
-    'University of Washington': 'washington',
-    'Yale': 'yale'
-}
 
-sinopia_groups_select = widgets.Select(
-   value=None,
-   options=sinopia_groups,
-   placeholder='Enter Sinopia group',
-   description='Group:',
-   disabled=False
-)
+@cache
+def get_groups(sinopia_api: str) -> list:
+    groups_url = f"{sinopia_api}groups/"
+    groups_result = requests.get(groups_url)
+    groups = []
+    for row in groups_result.json()["data"]:
+        groups.append((row["label"], row["id"]))
+    return sorted(groups, key=lambda y: y[0])
+
 
 sinopia_env_dropdown = widgets.RadioButtons(
-    options=[('Development', 'https://api.development.sinopia.io/'),
-             ('Stage', 'https://api.stage.sinopia.io/'),
-             ('Production', 'https://api.sinopia.io/')],
-    description='Environment:',
-    disabled=False
+    options=[
+        ("Development", "https://api.development.sinopia.io/"),
+        ("Stage", "https://api.stage.sinopia.io/"),
+        ("Production", "https://api.sinopia.io/"),
+    ],
+    description="Environment:",
+    disabled=False,
 )
 
-def generate_url(env, groups):
+
+sinopia_groups_select = widgets.Select(
+    value=None,
+    placeholder="Enter Sinopia group",
+    description="Group:",
+    disabled=False,
+)
+
+
+def update_group_options(change):
+    sinopia_groups_select.options = get_groups(change.new)
+
+
+sinopia_env_dropdown.observe(update_group_options, names="value")
+
+
+def generate_url(env, groups=None):
     resource_url = f"{env}resource"
     if groups is None:
         print(resource_url)
@@ -64,21 +61,14 @@ def generate_url(env, groups):
 
 
 url_output = widgets.interactive_output(
-    generate_url, 
-    {'env': sinopia_env_dropdown,
-     'groups': sinopia_groups_select }
+    generate_url, {"env": sinopia_env_dropdown, "groups": sinopia_groups_select}
 )
 
 sinopia_api_group_widget = widgets.VBox(
     [widgets.HBox([sinopia_env_dropdown, sinopia_groups_select]), url_output]
 )
 
-def sinopia_api_group_widget_borked():
-    header = widgets.HTML("<h2>Sinopia API URL</h2><h3>Generator</h3>", 
-                          layout=widgets.Layout(height='auto'))
-    header.style.text_align='center'
 
-    
 
 BF_ENTITIES_QUERY = """PREFIX bf: <>
 
